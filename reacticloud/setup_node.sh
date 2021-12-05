a18b16aeca1eef69adac98dac41e5e1fcf4be951
Donald Pinckney <donald_pinckney@icloud.com>
2021-12-04 19:17:10 -0500

    setup node script

---
+++ reacticloud/setup_node.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+
+
+if [ ! -f ~/.profile ]
+then
+	touch ~/.profile
+fi
+
+read -r -d '' PROFILE_CODE <<'EOF'
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+EOF
+
+echo "$PROFILE_CODE" >> ~/.profile
+
+nvm install 15.2.1
